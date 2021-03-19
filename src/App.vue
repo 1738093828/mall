3fca5b9935a2a27e52a8e363baadccfec7b56ce2
张雯雯 <1738093828@qq.com>
2021-03-19 17:11:48 +0800

    '修改'

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
-    <h1>大家好</h1>
+    <!--进行数据的显示-->
+    <router-view/>
+    <!--大写转小写中间加— -->
+    <main-tab-bar/>
   </div>
 </template>
+<script>
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar
+    }
+  }
+</script>
 <style>
   @import "./assets/css/base.css";
 </style>
